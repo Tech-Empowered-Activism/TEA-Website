f573095413ccca4e3923d54bfa0944302830c6cf
jeffDevelops <jefferydreynolds@gmail.com>
2022-04-13 22:07:48 -0600

    deploy script

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
+// next.config.js
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
+  assetPrefix: isProd ? '/your-github-repo-name/' : '',
   reactStrictMode: true,
 }
 
