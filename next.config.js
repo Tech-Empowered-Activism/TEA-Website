b035bd3c516f2e02d5de63a8ea0b8dd1486cbf6b
jeffDevelops <jefferydreynolds@gmail.com>
2022-04-14 11:48:34 -0600

    updates prod repo name

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 /** @type {import('next').NextConfig} */
 module.exports = {
-  assetPrefix: isProd ? '/tech-empowered-activism/' : '',
+  assetPrefix: isProd ? '/TEA-Website/' : '',
   reactStrictMode: true,
   // swcMinify: false,
 }
