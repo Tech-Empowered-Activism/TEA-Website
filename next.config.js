c771abb1bff325c4db78e36719dea9f3a599650d
Jeff <jefferydreynolds@gmail.com>
2022-05-02 21:08:58 -0600

    removes asset prefixing for prod, now that GH Pages is pointed to custom domain

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const isProd = process.env.NODE_ENV === 'production'
 
 /** @type {import('next').NextConfig} */
 module.exports = {
-  assetPrefix: isProd ? '/TEA-Website/' : '',
   reactStrictMode: true,
   // swcMinify: false,
 }
