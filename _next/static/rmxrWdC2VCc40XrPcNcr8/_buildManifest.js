f8011606e0dd869d6eadb6be8a4cd517ccd0a065
jeffDevelops <jeffDevelops@users.noreply.github.com>
2022-05-03 06:14:01 +0000

    deploy: 100091334e7db3d895182d751245c20cee94c85c

--- _next/static/rmxrWdC2VCc40XrPcNcr8/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/0ea204af10443866.css","static/chunks/pages/index-26f2f5e1b6be716f.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
